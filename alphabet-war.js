0a5e85d046480eba6b8632d412aa89747a0548fa
Andrew Ayetey <ayeteya@gmail.com>
2020-05-14 15:51:19 -0500

    first commit

---
+++ alphabet-war.js
@@ -0,0 +1,17 @@
+function leftCounter(string){
+  total = 0
+  for (i in string){
+    let letter = string[i]
+    if (letter == "w"){
+      total += 4
+    }
+    else if (letter == "p")
+  }
+}
+
+
+}
+function alphabetWar(fight)
+{
+   return "Let's fight again!";
+}
